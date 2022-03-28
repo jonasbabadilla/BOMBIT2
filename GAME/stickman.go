d6ea2a81593c49c1015aefb1e3a3db5d6827582d
Jonas Babadilla <jonasbabadilla@gmail.comm>
2022-03-27 19:53:56 -0500

    stickman

---
+++ GAME/stickman.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+type player struct{
+	texture *sdl.Texture
+}
+
+func Stickman(rednerer *sdl.Renderer) (p player, err error) {
+	var p player
+	
+	img, err := sdl.LoadBMP("SPRITES/stickman.bmp")
+	if err != nil {
+		return player{}, fmt.Errorf("loading player sprite:", err)
+	}
+	
+	defer img.Free() 
+	p.texture, err := renderer.CreateTextureFromSurface(img)
+	if err != nil {
+		return player{}, fmt.Errof("loading player texture:", err)
+	}
+	return p, nil
+}
