a9894a8622226e26c51cafce3e338d923b6ccf8e
Jonas Babadilla <jonasbabadilla@gmail.comm>
2022-03-29 08:18:44 -0500

    BAR

--- GAME/plr.go
+++ GAME/plr.go
@@ -28,19 +28,20 @@ func newPlayer(renderer *sdl.Renderer) (p player, e error) {
 		return player{}, fmt.Errorf("loading player texture: %v", err)
 	}
 
-	p.x = 640 /// 2.0
-	p.y = 360 //- playerSize/2.0
-
+	p.x = screenWidth/2.0 - playerSize
+	p.y = screenHeight/2.0 - playerSize/2.0
 	return p, nil
 }
 
 func (p *player) Draw(renderer *sdl.Renderer) {
+	// For some reason when I use this instead of p.x/p.y in Copy, the whole thing doesn't work anymore
+	// will need to look over that
 	//x := playerSize / 2.0
 	//y := playerSize / 2.0
 
 	renderer.Copy(p.Tex,
-		&sdl.Rect{X: 0, Y: 0, W: 32, H: 32},
-		&sdl.Rect{X: int32(p.x), Y: int32(p.y), W: 32, H: 32})
+		&sdl.Rect{X: 0, Y: 0, W: 64, H: 64},
+		&sdl.Rect{X: int32(p.x), Y: int32(p.y), W: 64, H: 64})
 
 }
 
@@ -50,7 +51,10 @@ func (p *player) Update() {
 	if keys[sdl.SCANCODE_LEFT] == 1 {
 		p.x -= playerSpeed
 	} else if keys[sdl.SCANCODE_RIGHT] == 1 {
+
 		p.x += playerSpeed
 	}
 
 }
+
+// renderer.CopyEx() another important function, has other parameters like center point, rotate, flip, etc
