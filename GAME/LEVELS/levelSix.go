0278d6061a870615745c2db5328526df264027b3
Jonas Babadilla <jonasbabadilla@gmail.comm>
2022-04-05 01:27:12 -0500

    level 5 and 6 sprites

---
+++ GAME/LEVELS/levelSix.go
@@ -0,0 +1,36 @@
+package levels
+
+import (
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func LevelSix(renderer *sdl.Renderer) (levelData []Object, LevelBG Object, pStart StartData, err error) {
+
+	Surf, _ := sdl.LoadBMP("LEVELS/LevelSixSprites/levelLayout.bmp")
+	Tex, _ := renderer.CreateTextureFromSurface(Surf)
+
+	levelData = CreateLevel(Surf, Tex)
+
+	defer Surf.Free()
+
+	Surf, _ = sdl.LoadBMP("LEVELS/LevelSixSprites/BG.bmp")
+	BG, _ := renderer.CreateTextureFromSurface(Surf)
+
+	backgroundData = Object{
+		Tex:          BG,
+		X:            0,
+		Y:            0,
+		ObjectWidth:  1280,
+		ObjectHeight: 720,
+	}
+
+	defer Surf.Free()
+
+	PlayerStart = StartData{X: 10, Y: 588, EndData: struct {
+		X int
+		Y int
+	}{X: 1184, Y: 652}}
+
+	return levelData, backgroundData, PlayerStart, nil
+
+}
